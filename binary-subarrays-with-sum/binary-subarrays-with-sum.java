140d451a801993cf1fc0c5ad5cbbcb0c823ce377
Yuan Chen <18861720589@163.com>
2021-07-09 14:25:11 -0400

    Time: 49 ms (12.87%), Space: 56 MB (6.31%) - LeetHub

---
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int sum = 0;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        int ret = 0;
+        for (int num : nums) {
+            cnt.put(sum, cnt.getOrDefault(sum, 0) + 1);
+            sum += num;
+            ret += cnt.getOrDefault(sum - goal, 0);
+        }
+        return ret;
+    }
+}
