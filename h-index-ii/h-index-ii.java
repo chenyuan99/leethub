8c33e8c036ffb413b39d0a4b5f0b9f60c07dca5e
Yuan Chen <18861720589@163.com>
2021-07-12 03:13:38 -0400

    Time: 0 ms (100.00%), Space: 45.9 MB (74.84%) - LeetHub

---
+++ h-index-ii/h-index-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return n - left;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/h-index-ii/solution/h-zhi-shu-ii-by-leetcode-solution-si7h/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
