d68e3f468dca4e0da5daf06a4cf483cec7db69f0
Yuan Chen <18861720589@163.com>
2021-07-21 14:16:20 -0400

    Time: 0 ms (100.00%), Space: 38.8 MB (44.09%) - LeetHub

---
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -0,0 +1,21 @@
+class Solution {
+    Map<Node, Node> cachedNode = new HashMap<Node, Node>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        if (!cachedNode.containsKey(head)) {
+            Node headNew = new Node(head.val);
+            cachedNode.put(head, headNew);
+            headNew.next = copyRandomList(head.next);
+            headNew.random = copyRandomList(head.random);
+        }
+        return cachedNode.get(head);
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/copy-list-with-random-pointer/solution/fu-zhi-dai-sui-ji-zhi-zhen-de-lian-biao-rblsf/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
