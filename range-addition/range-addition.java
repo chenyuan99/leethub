fce284e8ce87384879203228d48d3f13b162ea4b
Yuan Chen <18861720589@163.com>
2021-07-22 19:31:26 -0400

    Time: 1 ms (100.00%), Space: 45.3 MB (98.04%) - LeetHub

--- range-addition/range-addition.java
+++ range-addition/range-addition.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] getModifiedArray(int length, int[][] updates) {
+        if(length==0) return new int[length];
         int[] ans = new int[length];
-        for(int[] update:updates){
-            // ans[update[0]] = update[2] + ans[update[0] - 1];
-            // if (update[1] < length) {
-            //     ans[update[1]] = ans[update[1]] - update[2];
-            // }
-            for(int i = update[0];i<=update[1];i++){
-                ans[i] += update[2];
-            }
+        for(int[] update : updates){
+            int start = update[0];
+            int end = update[1];
+            int add = update[2];
+            if(start >= 0)  ans[start] += add;
+            if(end < length-1)  ans[end+1] -= add;   
         }
-        // for(int i =1;i<length;i++) {
-        //     ans[i] =  ans[i] + ans[i-1];
-        // }
+        for(int i = 0; i < length;i++){
+           if(i != 0) ans[i] = ans[i] + ans[i-1];
+        }
         return ans;
     }
 }
+
+// 作者：LaoGanMaIsEverything
+// 链接：https://leetcode-cn.com/problems/range-addition/solution/9938hen-jian-dan-by-laoganmaiseverything-x0jb/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
