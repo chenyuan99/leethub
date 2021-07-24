5a79efa34f0db67d0ae211e211012b84934feda3
Yuan Chen <18861720589@163.com>
2021-07-24 14:51:08 -0400

    Time: 0 ms (100.00%), Space: 39 MB (56.03%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //dynamic programming
+        int max = nums[0];
+		int pre = 0;
+        for(int num:nums){
+            pre=Math.max(pre+num,num);
+            max =Math.max(pre,max);
+        }
+		return max;
+    }
+}
