fcd18876a5035180a3de86e58bc10f8967f1889c
Yuan Chen <18861720589@163.com>
2021-07-26 10:58:18 -0400

    Time: 86 ms (52.73%), Space: 66.4 MB (21.82%) - LeetHub

---
+++ minimum-operations-to-make-a-subsequence/minimum-operations-to-make-a-subsequence.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int minOperations(int[] target, int[] arr) {
+        int n = target.length;
+        Map<Integer, Integer> pos = new HashMap<Integer, Integer>();
+        for (int i = 0; i < n; ++i) {
+            pos.put(target[i], i);
+        }
+        List<Integer> d = new ArrayList<Integer>();
+        for (int val : arr) {
+            if (pos.containsKey(val)) {
+                int idx = pos.get(val);
+                int it = binarySearch(d, idx);
+                if (it != d.size()) {
+                    d.set(it, idx);
+                } else {
+                    d.add(idx);
+                }
+            }
+        }
+        return n - d.size();
+    }
+
+    public int binarySearch(List<Integer> d, int target) {
+        int size = d.size();
+        if (size == 0 || d.get(size - 1) < target) {
+            return size;
+        }
+        int low = 0, high = size - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (d.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/minimum-operations-to-make-a-subsequence/solution/de-dao-zi-xu-lie-de-zui-shao-cao-zuo-ci-hefgl/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
