aca15994ac62a72c89274e54155165793166e10e
Yuan Chen <18861720589@163.com>
2021-11-22 12:27:44 -0600

    Time: 0 ms (100.00%), Space: 37.9 MB (8.42%) - LeetHub

---
+++ reaching-points/reaching-points.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx >= sx && ty >= sy) {
+            if (tx == ty) break;
+            if (tx > ty) {
+                if (ty > sy) tx %= ty;
+                else return (tx - sx) % ty == 0;
+            } else {
+                if (tx > sx) ty %= tx;
+                else return (ty - sy) % tx == 0;
+            }
+        }
+        return (tx == sx && ty == sy);
+    }
+}
+
+// 作者：LeetCode
+// 链接：https://leetcode-cn.com/problems/reaching-points/solution/dao-da-zhong-dian-by-leetcode/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
