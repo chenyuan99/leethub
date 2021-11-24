4deef798b7313a87462a97e6e9bcae5e3d7f5bb5
Yuan Chen <18861720589@163.com>
2021-11-25 03:41:22 +0800

    Time: 9 ms (26.90%), Space: 42.3 MB (8.37%) - LeetHub

---
+++ meeting-rooms/meeting-rooms.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canAttendMeetings(int[][] intervals) {
+         Arrays.sort(intervals, new Comparator<int[]>() {
+      public int compare(int[] i1, int[] i2) {
+        return i1[0] - i2[0];
+      }
+    });
+        for(int i =0; i <intervals.length-1;i++){
+            if(intervals[i][1] > intervals[i+1][0]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
