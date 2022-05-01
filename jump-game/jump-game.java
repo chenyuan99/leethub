cfd095b5f006d41e0dbcd73e65ac3e42cb2d1500
Yuan Chen <18861720589@163.com>
2022-05-01 12:16:21 -0500

    Time: 4 ms, Memory: 66.9 MB - LeetHub

---
+++ jump-game/jump-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int rightmost = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i <= rightmost) {
+                rightmost = Math.max(rightmost, i + nums[i]);
+                if (rightmost >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/jump-game/solution/tiao-yue-you-xi-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
