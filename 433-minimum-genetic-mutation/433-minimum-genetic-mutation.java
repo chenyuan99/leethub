1c832805540aefb3a686afbb0b1a1bb505edc9c3
Yuan Chen <18861720589@163.com>
2022-05-07 07:28:48 -0500

    Time: 0 ms (100.00%), Space: 40.2 MB (86.22%) - LeetHub

---
+++ 433-minimum-genetic-mutation/433-minimum-genetic-mutation.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int minMutation(String start, String end, String[] bank) {
+        int m = start.length();
+        int n = bank.length;
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+        int endIndex = -1;
+        for (int i = 0; i < n; i++) {
+            if (end.equals(bank[i])) {
+                endIndex = i;
+            }
+            for (int j = i + 1; j < n; j++) {
+                int mutations = 0;
+                for (int k = 0; k < m; k++) {
+                    if (bank[i].charAt(k) != bank[j].charAt(k)) {
+                        mutations++;
+                    }
+                    if (mutations > 1) {
+                        break;
+                    }
+                }
+                if (mutations == 1) {
+                    adj[i].add(j);
+                    adj[j].add(i);
+                }
+            }
+        }
+        if (endIndex == -1) {
+            return -1;
+        }
+
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        boolean[] visited = new boolean[n];
+        int step = 1;
+        for (int i = 0; i < n; i++) {
+            int mutations = 0;
+            for (int k = 0; k < m; k++) {
+                if (start.charAt(k) != bank[i].charAt(k)) {
+                    mutations++;
+                }
+                if (mutations > 1) {
+                    break;
+                }
+            }
+            if (mutations == 1) {
+                queue.offer(i);
+                visited[i] = true;
+            }
+        }        
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                int curr = queue.poll();
+                if (curr == endIndex) {
+                    return step;
+                }
+                for (int next : adj[curr]) {
+                    if (visited[next]) {
+                        continue;
+                    }
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode.cn/problems/minimum-genetic-mutation/solution/zui-xiao-ji-yin-bian-hua-by-leetcode-sol-lhwy/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
