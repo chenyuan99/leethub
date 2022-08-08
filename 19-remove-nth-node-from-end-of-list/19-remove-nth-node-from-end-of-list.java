64a4ccf804d824bae564659ddb74fb2d8d532671
Yuan Chen <yc149@rice.edu>
2022-08-07 17:24:30 -0700

    Time: 1 ms (72.63%), Space: 42.9 MB (7.88%) - LeetHub

---
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n-- > 0) {
+            fast = fast.next;
+        }
+        // 记住 待删除节点slow 的上一节点
+        ListNode prev = null;
+        while (fast != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // 上一节点的next指针绕过 待删除节点slow 直接指向slow的下一节点
+        prev.next = slow.next;
+        // 释放 待删除节点slow 的next指针, 这句删掉也能AC
+        slow.next = null;
+
+        return dummy.next;
+    }
+}
+
+
+// 作者：carlsun-2
+// 链接：https://leetcode.cn/problems/remove-nth-node-from-end-of-list/solution/by-carlsun-2-1jhp/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
