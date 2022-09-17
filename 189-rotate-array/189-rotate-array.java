4e05c24992bece1de6404eae904a86a09f237089
Yuan Chen <yc149@rice.edu>
2022-09-17 18:16:57 -0500

    Time: 3 ms (28.95%), Space: 65.2 MB (6.54%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/rotate-array/solution/xuan-zhuan-shu-zu-by-leetcode-solution-nipk/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
