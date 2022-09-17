5c7f0312c3793e3a49946ce7632c6e66a05cc154
Yuan Chen <yc149@rice.edu>
2022-09-17 13:04:39 -0500

    Time: 1 ms (63.16%), Space: 42.4 MB (19.62%) - LeetHub

---
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Deque<Integer> leftStack = new LinkedList<Integer>();
+        Deque<Integer> asteriskStack = new LinkedList<Integer>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                leftStack.push(i);
+            } else if (c == '*') {
+                asteriskStack.push(i);
+            } else {
+                if (!leftStack.isEmpty()) {
+                    leftStack.pop();
+                } else if (!asteriskStack.isEmpty()) {
+                    asteriskStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        while (!leftStack.isEmpty() && !asteriskStack.isEmpty()) {
+            int leftIndex = leftStack.pop();
+            int asteriskIndex = asteriskStack.pop();
+            if (leftIndex > asteriskIndex) {
+                return false;
+            }
+        }
+        return leftStack.isEmpty();
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/valid-parenthesis-string/solution/you-xiao-de-gua-hao-zi-fu-chuan-by-leetc-osi3/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
