ad4632a372fdb36c3ab4f8c0208451fe615c1f28
Yuan Chen <cysbc1999@gmail.com>
2023-10-16 08:22:01 -0400

    Time: 10 ms, Memory: 55.7 MB - LeetHub

---
+++ 0kth-largest-element-in-an-array/0kth-largest-element-in-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    int quickselect(int[] nums, int l, int r, int k) {
+        if (l == r) return nums[k];
+        int x = nums[l], i = l - 1, j = r + 1;
+        while (i < j) {
+            do i++; while (nums[i] < x);
+            do j--; while (nums[j] > x);
+            if (i < j){
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+        if (k <= j) return quickselect(nums, l, j, k);
+        else return quickselect(nums, j + 1, r, k);
+    }
+    public int findKthLargest(int[] _nums, int k) {
+        int n = _nums.length;
+        return quickselect(_nums, 0, n - 1, n - k);
+    }
+}
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/kth-largest-element-in-an-array/solutions/307351/shu-zu-zhong-de-di-kge-zui-da-yuan-su-by-leetcode-/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
